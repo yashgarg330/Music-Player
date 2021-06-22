a0aaf1bc0bb5180ea27f1c5cd03e4af8889e836c
Yash <yashgarg0905@gmail.com>
2021-06-22 18:40:49 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import react from "react";
+import React from "react";
+//Adding Styles
+import "./styles/App.scss";
+//Adding Components
+import Player from "./components/Player";
+import Song from "./components/Song";
 
-function App() {
-  return (
-    <div>
-      <h1>Music Player</h1>
-    </div>
-  );
+
+const App = () => {
+    return (
+        <div>
+            <Song />
+            <Player />
+        </div>
+    );
 }
 
 export default App;
